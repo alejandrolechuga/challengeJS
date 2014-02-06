328ce5c569d5af862bd5e67fc95ac6c54e6af8a5
Alejandro Amador <ramon.lechuga@caliente.com.mx>
2014-02-06 13:19:41 -0800

    Adding operators

--- HTML/calculator/calculator.js
+++ HTML/calculator/calculator.js
@@ -1,4 +1,42 @@
+/**
+* @todo Basic operations
+* @todo Display results
+*/
 (function(window, document, undefined) {
   "use strict";
-  
+
+  var calculator = function () {
+    this.firstvalue = 0;
+    this.secondvalue = 0;
+    this.currentoperator;
+  };
+
+  calculator.prototype = {
+    input: function (number) {
+
+    },
+
+    result :function () {
+       switch (operator) {
+        case "+" : 
+          this.currentoperator = "+";
+        break;
+        case "-" :
+        break;
+        case "/" :
+        break;
+        case "*" :
+        break;
+      }
+      return currentvalue;
+    },
+
+    operation : function (operator) {
+      this.currentoperator = operator;
+    }
+  };
+  calculator.input(0);
+  calculator.operation("+");
+  calculator.input(2342);
+  calculator.result();
 }(this, document));
