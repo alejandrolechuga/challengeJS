1246e0e1a23872065ea8f34e3e40c6a7f4f39ff9
Alejandro Amador <ramon.lechuga@caliente.com.mx>
2014-02-06 16:42:22 -0800

    Performing operation

--- HTML/calculator/calculator.js
+++ HTML/calculator/calculator.js
@@ -9,13 +9,20 @@
     this.firstvalue = 0;
     this.secondvalue = 0;
     this.currentoperator;
+    this.performing = false;
   };
 
   calculator.prototype = {
     input: function (number) {
+      if (this.performing) {
+        this.secondavalue = number;
+      } else {
+        this.firstvalue = number;
+      }
+    },
+    clear: function () {
 
     },
-
     result :function () {
        switch (operator) {
         case "+" : 
